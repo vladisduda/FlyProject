e40590d7b3999d132a12a2234db5b77d69494548
vladisduda <vladisduda@mail.ru>
2023-12-03 19:32:33 +0300

    Выполнил СОЗДАНИЕ МОДЕЛИ ДАННЫХ ПРИЛОЖЕНИЯ

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/testik')
+mongoose.connect('mongodb://127.0.0.1:27017/test1')
+var Fly = require('./models/fly').Fly
 
 var schema = mongoose.Schema({ name: String })
 
-schema.methods.scholoh = function () {
-	console.log(this.get('name') + ' подумал, покушал и лег спать')
-}
-
-const Gif = mongoose.model('Gif', schema)
-
-const Meme = new Gif({ name: 'Рома' })
-Meme.save()
-	.then(function () {
-		Meme.scholoh()
-	})
-	.catch(function (err) {
-		console.error(err)
-	})
+var fly = new Fly({
+	title: 'Чмоня',
+	nick: 'Chmonya',
+})
+console.log(fly)
+fly.save().then(function (err, fly, affected) {
+	console.log(fly.title)
+})
