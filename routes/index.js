2e7ea7c6d85a504ae94336e13210e9d0eca13f41
vladisduda <vladisduda@mail.ru>
2023-12-13 16:26:24 +0300

    Чистка кода и немного изменение начальной страницы

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,10 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' })
+	res.render('index', {
+		title: 'Пользователь',
+		picture: 'images/hallo.gif',
+	})
 })
 
-router.get('/sleep', function (req, res, next) {
-	res.render('gif', {
-		title: 'Сон',
-		picture: 'images/sleep.gif',
-		desc: 'Естественное периодическое состояние покоя и расслабления организма, сопровождающееся минимальным уровнем сознательной деятельности.',
-	})
-})
-router.get('/eats', function (req, res, next) {
-	res.render('gif', {
-		title: 'Прием пищи',
-		picture: 'images/eats.gif',
-		desc: 'Процесс принятия готовой пищи, совершаемый в определённый период времени, обычно несколько раз в сутки.',
-	})
-})
-router.get('/thinking', function (req, res, next) {
-	res.render('gif', {
-		title: 'Размышление',
-		picture: 'images/thinking.gif',
-		desc: 'Процесс обдумывания или размышления над какой-либо темой или идеей.',
-	})
-})
 module.exports = router
