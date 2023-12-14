621e1a79ff8c9364c1cfe9205c21bbeef8554cab
vladisduda <vladisduda@mail.ru>
2023-12-14 12:20:14 +0300

    УСТАНОВКА МОДУЛЯ EXPRESS-SESSION И НАСТРОЙКА COOKIE

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/fly')
+var session = require('express-session')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -24,6 +25,14 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(
+	session({
+		secret: 'fly',
+		cookie: { maxAge: 60 * 1000 },
+		resave: true,
+		saveUninitialized: true,
+	})
+)
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/gifs', gifsRouter)
